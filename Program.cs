1c2d991ca9c523b28b11c0f46972554fcbf1237c
BenjaminVilloldo <benjaminvillol@gmail.com>
2025-05-20 16:20:24 -0500

    invertir numero en proceso

--- Program.cs
+++ Program.cs
@@ -2,7 +2,30 @@
 Console.WriteLine("Hello, World!");
 int a;
 int b;
-a=10;
-b=a;
-Console.WriteLine("valor de a:"+a);
-Console.WriteLine("valor de b:"+b);
+a = 10;
+b = a;
+Console.WriteLine("valor de a:" + a);
+Console.WriteLine("valor de b:" + b);
+
+Console.WriteLine("--CALCULADORA--\nIngrese el valor: ");
+
+int numero = 0;
+numero = Console.Read();
+
+int numeroInvertido = 0;
+int ultimoDigito;
+int numSalida;
+if (int.TryParse(int numero, out numSalida))
+{
+    
+}
+do
+{
+
+
+    ultimoDigito = numero % 10;
+    numeroInvertido = (numeroInvertido * 10) + ultimoDigito;
+    numero /= 10;
+} while (numero > 0);
+
+Console.WriteLine("El numero invertido es: " + numeroInvertido);
