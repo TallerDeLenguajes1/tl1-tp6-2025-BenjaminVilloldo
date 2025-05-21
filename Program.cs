393b6ee27b2dbe7ed14ec41b3d7a512619e675ed
BenjaminVilloldo <benjaminvillol@gmail.com>
2025-05-21 12:49:17 -0300

    Terminado el punto 1

--- Program.cs
+++ Program.cs
@@ -6,20 +6,27 @@ a = 10;
 b = a;
 Console.WriteLine("valor de a:" + a);
 Console.WriteLine("valor de b:" + b);
+
 Console.WriteLine("--CALCULADORA--\nIngrese el valor: ");
 
-int numero = 0;
-numero = Console.Read();
-
+string? entrada = Console.ReadLine();
+int numero;
 int numeroInvertido = 0;
 int ultimoDigito;
 
+if (int.TryParse(entrada, out numero))
 {
+    do
+    {
+        ultimoDigito = numero % 10; // toma el ultimo digito
+        numeroInvertido = (numeroInvertido * 10) + ultimoDigito;
+        numero /= 10;
+    } while (numero > 0);
+    Console.WriteLine("El numero invertido es: " + numeroInvertido);
+
+}else
+{
+    Console.WriteLine("No se ingreso un numero, reinicie el sistema.");
+}
 
 
-    ultimoDigito = numero % 10;
-    numeroInvertido = (numeroInvertido * 10) + ultimoDigito;
-    numero /= 10;
-} while (numero > 0);
-
-Console.WriteLine("El numero invertido es: " + numeroInvertido);
